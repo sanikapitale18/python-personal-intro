18c4308e902a43ef78c1e3fd78944c32f5566e62
sanikapitale18 <sanikapitale18@gmail.com>
2025-12-17 23:11:47 +0530

    Create personal_intro.py

---
+++ personal_intro.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+hobby = input("Enter your hobby: ")
+
+print("\nWelcome!")
+print("Hello", name)
+print("You are", age, "years old and you enjoy", hobby)
+print("Nice to meet you!")
+
+
